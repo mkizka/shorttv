edada6ec425fdde9a930606f6036b87c2ec207ba
mkizka <g.mkizka@gmail.com>
2022-09-24 01:07:52 +0900

    staleTimeを無限に

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,9 @@ import { ClipPlayerContainer } from "../components/ClipPlayerContainer";
 import { trpc } from "../utils/trpc";
 
 const Index: NextPage = () => {
-  const clips = trpc.useQuery(["main.getClips"]);
+  // ClipPlayerContainerのfocusをiframeから剥がす処理で、
+  // なぜかtrpcの再読み込みが呼ばれるので二度以上通信しないようにする
+  const clips = trpc.useQuery(["main.getClips"], { staleTime: Infinity });
 
   return (
     <main className="flex flex-col h-full bg-black">
